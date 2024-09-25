c1c3a033eb15608760a096c6ac2bfe735f6444e5
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-09-25 10:07:19 -0500

    updated documentation

--- src/CSVHelper.java
+++ src/CSVHelper.java
@@ -5,6 +5,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class CSVHelper {
+	/**
+	 * Creates a CSVReader object for reading from a CSV file.
+	 *
+	 * @param filename The name of the CSV file to read from. Path starts from root
+	 *                 directory of the project.
+	 * @return A CSVReader object for reading from the specified file, or null if an
+	 *         error occurs.
+	 * @throws Exception If an error occurs while creating the CSVReader.
+	 */
 	public static CSVReader createCSVReader(String filename) {
 		try {
 			CSVReader reader = new CSVReader(new FileReader(filename));
@@ -15,6 +24,15 @@ public class CSVHelper {
 		}
 	}
 
+	/**
+	 * Creates a CSVWriter object for writing to a CSV file.
+	 *
+	 * @param filename The name of the CSV file to write to. Path starts from root
+	 *                 directory of the project.
+	 * @return A CSVWriter object for writing to the specified file, or null if an
+	 *         error occurs.
+	 * @throws Exception If an error occurs while creating the CSVWriter.
+	 */
 	public static CSVWriter createCSVWriter(String filename) {
 		try {
 			CSVWriter writer = new CSVWriter(new FileWriter(filename));
@@ -24,5 +42,5 @@ public class CSVHelper {
 			return null;
 		}
 	}
-	
+
 }
