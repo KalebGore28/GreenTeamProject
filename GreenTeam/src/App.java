4a7947e233e5478419350c10074ca338a912c1e7
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-09-25 09:25:50 -0500

    push

--- GreenTeam/src/App.java
+++ GreenTeam/src/App.java
@@ -1,19 +1,15 @@
 import com.opencsv.CSVReader;
-import com.opencsv.CSVParserBuilder;
 import java.io.FileReader;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CSVReader reader = new CSVReader(new FileReader("yourfile.csv"),
-            new CSVParserBuilder()
-                .withSeparator('\t')
-                .build());
-        
-        // Example of reading data
-        List<String[]> records = reader.readAll();
-        for (String[] record : records) {
-            System.out.println(String.join(", ", record));
+        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        List<String[]> data = reader.readAll();
+        for (String[] row : data) {
+            System.out.println(row[0] + " " + row[1] + " " + row[2]);
         }
+        reader.close();
+
     }
 }
