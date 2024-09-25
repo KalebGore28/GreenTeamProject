35c556d69de6e9f4b9f414b0fdd7a129d51ff890
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-09-25 09:46:33 -0500

    cleaning

--- src/App.java
+++ src/App.java
@@ -1,22 +1,5 @@
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
-import java.io.FileReader;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        CSVReader reader = new CSVReaderBuilder(new FileReader("src/data.csv")).build();
-
-        List<String[]> allLines = reader.readAll();
-
-        for (String[] nextLine : allLines) {
-            for (String token : nextLine) {
-                System.out.print(token + " ");
-            }
-            System.out.println();
-        }
-
-        reader.close();
+        System.out.println("Hello, World!");
     }
 }
