ea4e8cf261b6d90114951aec865026e2b5a391f9
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-09-25 09:44:45 -0500

    adding dependencies

--- src/App.java
+++ src/App.java
@@ -1,15 +1,22 @@
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
 import java.io.FileReader;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CSVReader reader = new CSVReader(new FileReader("src/data.csv"));
-        List<String[]> data = reader.readAll();
-        for (String[] row : data) {
-            System.out.println(row[0] + " " + row[1] + " " + row[2]);
-        }
-        reader.close();
+        CSVReader reader = new CSVReaderBuilder(new FileReader("src/data.csv")).build();
 
+        List<String[]> allLines = reader.readAll();
+
+        for (String[] nextLine : allLines) {
+            for (String token : nextLine) {
+                System.out.print(token + " ");
+            }
+            System.out.println();
+        }
+
+        reader.close();
     }
 }
