305f67a78eae267ee4fdb77b98fe90f6960e0b77
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-09-25 09:11:12 -0500

    push

--- GreenTeam/src/App.java
+++ GreenTeam/src/App.java
@@ -1,5 +1,19 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVParserBuilder;
+import java.io.FileReader;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CSVReader reader = new CSVReader(new FileReader("yourfile.csv"),
+            new CSVParserBuilder()
+                .withSeparator('\t')
+                .build());
+        
+        // Example of reading data
+        List<String[]> records = reader.readAll();
+        for (String[] record : records) {
+            System.out.println(String.join(", ", record));
+        }
     }
 }
